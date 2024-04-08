bef2fec45f268f6eff144eb33a7e4f32c360495b
voidful <voidful.stack@gmail.com>
2024-04-09 02:49:55 +0800

    init commit

---
+++ init.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+from time import sleep
+import os
+
+import pandas as pd
+import vectorbt as vbt
+import twstock
+
+codes = twstock.codes
+
+
+def get_past_x_days(stock_num, days=59):
+    csv_path = f'./data/{stock_num}.csv'
+
+    if os.path.exists(csv_path):
+        try:
+            df = pd.read_csv(csv_path)
+            if not df.empty and len(df) > 1:  # 檔案非空且超過一行
+                print(f"Data for {stock_num} already exists and is not empty. Skipping download.")
+                return
+        except pd.errors.EmptyDataError:
+            pass
+
+    today = datetime.now()
+
+    start_date = today - timedelta(days=days)
+
+    end_date = today
+
+    start_date_str = start_date.strftime('%Y-%m-%d')
+    end_date_str = end_date.strftime('%Y-%m-%d')
+
+    yf_data = vbt.YFData.download(
+        f"{stock_num}.TW",
+        start=start_date_str + " 08:30:00 +0800",
+        end=end_date_str + " 13:30:00 +0800",
+        interval='5m',
+        missing_index='drop'
+    )
+
+    data = yf_data.get()
+    data.to_csv(csv_path)
+
+
+for k, v in codes.items():
+    if v.market == '上市' and v.type == '股票':
+        get_past_x_days(k)
+        print(v)
---
+++ update.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+from time import sleep
+import os
+import pandas as pd
+
+import vectorbt as vbt
+import twstock
+
+codes = twstock.codes
+
+
+def get_data_since_last_record(stock_num, base_path='./data/'):
+    csv_path = f'{base_path}{stock_num}.csv'
+    today = datetime.now()
+
+    if os.path.exists(csv_path):
+        data = pd.read_csv(csv_path)
+        if not data.empty:
+            last_record_date = pd.to_datetime(data['Datetime'].iloc[-1])
+            start_date = last_record_date + timedelta(minutes=5)
+        else:
+            start_date = today - timedelta(days=59)
+    else:
+        start_date = today - timedelta(days=59)
+
+    end_date = today
+
+    start_date_str = start_date.strftime('%Y-%m-%d %H:%M:%S +0800')
+    end_date_str = end_date.strftime('%Y-%m-%d %H:%M:%S +0800')
+
+    yf_data = vbt.YFData.download(
+        f"{stock_num}.TW",
+        start=start_date_str,
+        end=end_date_str,
+        interval='5m',
+        missing_index='drop'
+    )
+
+    new_data = yf_data.get()
+
+    if os.path.exists(csv_path):
+        new_data.to_csv(csv_path, mode='a', header=False, index=False)
+    else:
+        new_data.to_csv(csv_path, index=False)
+
+    return new_data
+
+
+for k, v in codes.items():
+    if v.market == '上市' and v.type == '股票':
+        new_data = get_data_since_last_record(k)
+        print(f"Updated data for {k}")
